4e012d32512a6564a08d5ee8c2f8a841a154886e
can019 <jys01012@gmail.com>
2025-08-21 20:15:33 +0900

    chore: fix lint

--- apps/user-service/src/main/java/com/gltkorea/icebang/domain/user/model/UserAccountPrincipal.java
+++ apps/user-service/src/main/java/com/gltkorea/icebang/domain/user/model/UserAccountPrincipal.java
@@ -1,26 +1,27 @@
 package com.gltkorea.icebang.domain.user.model;
 
-import lombok.Builder;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import java.util.Collection;
 import java.util.List;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.Builder;
+
 @Builder
 public class UserAccountPrincipal implements UserDetails {
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
-    }
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of();
+  }
 
-    @Override
-    public String getPassword() {
-        return "";
-    }
+  @Override
+  public String getPassword() {
+    return "";
+  }
 
-    @Override
-    public String getUsername() {
-        return "";
-    }
+  @Override
+  public String getUsername() {
+    return "";
+  }
 }
