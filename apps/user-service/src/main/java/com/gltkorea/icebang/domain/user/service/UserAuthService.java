d5c7839eab9f771da81d94a38f9e491af3066444
can019 <jys01012@gmail.com>
2025-08-21 19:01:13 +0900

    chore: init user auth service

---
+++ apps/user-service/src/main/java/com/gltkorea/icebang/domain/user/service/UserAuthService.java
@@ -0,0 +1,6 @@
+package com.gltkorea.icebang.domain.user.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserAuthService extends UserDetailsService {
+}
