2c05c581af0f85e1a8de3a9e7c69057adee002f4
can019 <jys01012@gmail.com>
2025-08-21 18:50:08 +0900

    chore: init sping boot user service

---
+++ apps/user-service/src/test/java/com/gltkorea/icebang/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.gltkorea.icebang;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
