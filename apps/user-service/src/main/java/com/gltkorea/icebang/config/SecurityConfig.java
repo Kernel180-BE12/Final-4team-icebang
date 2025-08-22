75f5b4ebb43e0095e01354fe8ea83b53d0400ea8
can019 <jys01012@gmail.com>
2025-08-22 13:45:32 +0900

    chore: Password encorder 설정

--- apps/user-service/src/main/java/com/gltkorea/icebang/config/SecurityConfig.java
+++ apps/user-service/src/main/java/com/gltkorea/icebang/config/SecurityConfig.java
@@ -1,6 +1,28 @@
 package com.gltkorea.icebang.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
-public class SecurityConfig {}
+@RequiredArgsConstructor
+public class SecurityConfig {
+  private final Environment environment;
+
+  @Bean
+  public PasswordEncoder bCryptPasswordEncoder() {
+    String[] activeProfiles = environment.getActiveProfiles();
+
+    for (String profile : activeProfiles) {
+      if ("dev".equals(profile)) {
+        return NoOpPasswordEncoder.getInstance();
+      }
+    }
+    return new BCryptPasswordEncoder();
+  }
+}
