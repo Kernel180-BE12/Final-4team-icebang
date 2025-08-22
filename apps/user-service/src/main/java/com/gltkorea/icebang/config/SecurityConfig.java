208484f4a9d776a24155827a6ef1d8e6303ba93e
can019 <jys01012@gmail.com>
2025-08-22 13:46:27 +0900

    chore: test환경 password encoder 설정

--- apps/user-service/src/main/java/com/gltkorea/icebang/config/SecurityConfig.java
+++ apps/user-service/src/main/java/com/gltkorea/icebang/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
     String[] activeProfiles = environment.getActiveProfiles();
 
     for (String profile : activeProfiles) {
-      if ("dev".equals(profile)) {
+      if ("dev".equals(profile) || "test".equals(profile)) {
         return NoOpPasswordEncoder.getInstance();
       }
     }
