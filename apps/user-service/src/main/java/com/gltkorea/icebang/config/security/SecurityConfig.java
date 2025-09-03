19e29bb469f13eae2d7b603ecd6fb7be8a580497
nghyeokSim <simjeonghyeok1020@gmail.com>
2025-09-03 16:24:51 +0900

    fix: AuthenticationManager bean 주입

--- apps/user-service/src/main/java/com/gltkorea/icebang/config/security/SecurityConfig.java
+++ apps/user-service/src/main/java/com/gltkorea/icebang/config/security/SecurityConfig.java
@@ -5,6 +5,8 @@ import java.security.SecureRandom;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -72,5 +74,11 @@ public class SecurityConfig {
       }
     }
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+      throws Exception {
+    return config.getAuthenticationManager();
   }
 }
