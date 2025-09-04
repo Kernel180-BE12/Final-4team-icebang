36d74aaecdb94a276eaf6b958428924c710dc04c
can019 <jys01012@gmail.com>
2025-09-04 20:44:38 +0900

    fix: OrganizationOptionsDto 직렬화
    getter/setter 추가

--- apps/user-service/src/main/java/com/gltkorea/icebang/domain/organization/dto/OrganizationOptionsDto.java
+++ apps/user-service/src/main/java/com/gltkorea/icebang/domain/organization/dto/OrganizationOptionsDto.java
@@ -6,9 +6,13 @@ import com.gltkorea.icebang.domain.department.dto.DepartmentsCardDto;
 import com.gltkorea.icebang.domain.position.dto.PositionCardDto;
 import com.gltkorea.icebang.domain.roles.dto.RolesCardDto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 
 @Builder
+@Data
+@AllArgsConstructor
 public class OrganizationOptionsDto {
   List<DepartmentsCardDto> departments;
   List<PositionCardDto> positions;
