f7f7b3f13a2b4cd58131ded67059d06b6269826a
can019 <jys01012@gmail.com>
2025-09-05 11:44:10 +0900

    chore: EmailService todo list 작성

--- apps/user-service/src/main/java/com/gltkorea/icebang/domain/email/service/EmailServiceImpl.java
+++ apps/user-service/src/main/java/com/gltkorea/icebang/domain/email/service/EmailServiceImpl.java
@@ -12,5 +12,13 @@ import lombok.RequiredArgsConstructor;
 @ConditionalOnMissingBean(EmailService.class)
 public class EmailServiceImpl implements EmailService {
   @Override
-  public void send(EmailRequest emailRequest) {}
+  public void send(EmailRequest emailRequest) {
+    /*
+     @TODO
+    * 1. icebang email 설정
+    * 2. email request 발송
+
+    고려사항: email 전송 관련 bean, dependency, 구글 계정 설정 등 필요
+    */
+  }
 }
