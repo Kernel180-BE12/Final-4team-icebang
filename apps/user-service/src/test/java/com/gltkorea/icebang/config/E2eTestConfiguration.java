425fbccae585a602bc666326a98785d465f497bf
can019 <jys01012@gmail.com>
2025-09-07 13:21:24 +0900

    chore: e2e test configuration 위치 이동
     Changes to be committed:
    	renamed:    src/test/java/com/gltkorea/icebang/E2eTestConfiguration.java -> src/test/java/com/gltkorea/icebang/config/E2eTestConfiguration.java

--- apps/user-service/src/test/java/com/gltkorea/icebang/config/E2eTestConfiguration.java
+++ apps/user-service/src/test/java/com/gltkorea/icebang/config/E2eTestConfiguration.java
@@ -1,4 +1,4 @@
-package com.gltkorea.icebang;
+package com.gltkorea.icebang.config;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
