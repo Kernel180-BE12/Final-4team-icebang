f7e1b11e2639447e5bdee87e513dfed2e1be7f7c
can019 <jys01012@gmail.com>
2025-09-07 13:25:43 +0900

    chore: unit, e2e test profile 설정

---
+++ apps/user-service/src/test/java/com/gltkorea/icebang/annotation/E2eTest.java
@@ -0,0 +1,10 @@
+package com.gltkorea.icebang.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface E2eTest {}
