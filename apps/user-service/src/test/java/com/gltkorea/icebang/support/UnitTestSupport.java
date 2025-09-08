b11889c71f5076323e76ac88667cd29ff5f72f7f
Yousung Jung <jys01012@gmail.com>
2025-09-08 11:20:41 +0900

    chore: E2E profile 변경 (#48)

--- apps/user-service/src/test/java/com/gltkorea/icebang/support/UnitTestSupport.java
+++ apps/user-service/src/test/java/com/gltkorea/icebang/support/UnitTestSupport.java
@@ -1,12 +1,11 @@
 package com.gltkorea.icebang.support;
 
-import org.junit.jupiter.api.Tag;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@Tag("unit")
+import com.gltkorea.icebang.annotation.UnitTest;
+
 @SpringBootTest
-@ActiveProfiles("test-unit")
+@UnitTest
 public abstract class UnitTestSupport {
 
   // 공통 유틸리티 메서드들
