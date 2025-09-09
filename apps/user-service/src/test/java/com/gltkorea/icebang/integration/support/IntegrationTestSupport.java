a7ad6adb393c10de8bd3676f0d384bf045026711
can019 <jys01012@gmail.com>
2025-09-10 01:01:38 +0900

    chore: integrationTest annotation 및 support 정의

---
+++ apps/user-service/src/test/java/com/gltkorea/icebang/integration/support/IntegrationTestSupport.java
@@ -0,0 +1,6 @@
+package com.gltkorea.icebang.integration.support;
+
+import com.gltkorea.icebang.integration.annotation.IntegrationTest;
+
+@IntegrationTest
+public abstract class IntegrationTestSupport {}
