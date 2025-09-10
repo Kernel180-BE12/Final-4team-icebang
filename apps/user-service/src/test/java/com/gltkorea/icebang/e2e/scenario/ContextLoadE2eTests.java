f45d00b3451ad0cd68934140939f0d2e07ac6750
can019 <jys01012@gmail.com>
2025-09-10 12:32:07 +0900

    chore: rename UserServiceApplicationE2eTest
     Changes to be committed:
    	renamed:    src/test/java/com/gltkorea/icebang/e2e/scenario/UserServiceApplicationE2eTests.java -> src/test/java/com/gltkorea/icebang/e2e/scenario/ContextLoadE2eTests.java

--- apps/user-service/src/test/java/com/gltkorea/icebang/e2e/scenario/ContextLoadE2eTests.java
+++ apps/user-service/src/test/java/com/gltkorea/icebang/e2e/scenario/ContextLoadE2eTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import com.gltkorea.icebang.e2e.support.E2eTestSupport;
 
-class UserServiceApplicationE2eTests extends E2eTestSupport {
+class ContextLoadE2eTests extends E2eTestSupport {
 
   @Test
   void contextLoads() {}
