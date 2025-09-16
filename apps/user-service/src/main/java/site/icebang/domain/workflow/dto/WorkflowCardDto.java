d5a766f835bc4a04e54d499741f2322386e2233a
bwnfo3 <bwnfo0702@gmail.com>
2025-09-16 17:53:54 +0900

    feat: WorkflowCardDto

--- apps/user-service/src/main/java/site/icebang/domain/workflow/dto/WorkflowCardDto.java
+++ apps/user-service/src/main/java/site/icebang/domain/workflow/dto/WorkflowCardDto.java
@@ -2,5 +2,15 @@ package site.icebang.domain.workflow.dto;
 
 import lombok.Data;
 
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+
 @Data
-public class WorkflowCardDto {}
+public class WorkflowCardDto {
+    private BigInteger id;
+    private String name;
+    private String description;
+    private boolean isEnabled;
+    private String createdBy;
+    private LocalDateTime createdAt;
+}
