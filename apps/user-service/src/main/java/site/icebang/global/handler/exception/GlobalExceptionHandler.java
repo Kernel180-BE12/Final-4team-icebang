5ca1bf3aaa1811804bb3314bce072afbb7c4768a
Yousung Jung <jys01012@gmail.com>
2025-09-17 16:31:22 +0900

    Spring boot Promtail을 통해 Loki log 전송 (#115)
    * chore: Loki Promtail을 통해 file 전송으로 변경
    
    * chore: Promtail을 통해 loki 전송 (local)
    
    * chore: Exception handler error, warn logging
    
    - warn: Data duplicate
    - error: Exception
    
    * fix: spanid logging 추가
    
    * chore: production grafana 설정
    
    * chore: Promtail 복사 ci 작성
    
    * chore: User-service promtail 이 후 container화 되도록 설정
    
    * chore: Production, grafana 연동 세팅

--- apps/user-service/src/main/java/site/icebang/global/handler/exception/GlobalExceptionHandler.java
+++ apps/user-service/src/main/java/site/icebang/global/handler/exception/GlobalExceptionHandler.java
@@ -9,10 +9,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
+import lombok.extern.slf4j.Slf4j;
+
 import site.icebang.common.dto.ApiResponse;
 import site.icebang.common.exception.DuplicateDataException;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
   @ExceptionHandler(MethodArgumentNotValidException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -24,6 +27,7 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(Exception.class)
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   public ApiResponse<String> handleGeneric(Exception ex) {
+    log.error(ex.getMessage(), ex);
     return ApiResponse.error("Internal error: ", HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
@@ -48,6 +52,7 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(DuplicateDataException.class)
   @ResponseStatus(HttpStatus.CONFLICT)
   public ApiResponse<String> handleDuplicateData(DuplicateDataException ex) {
+    log.warn(ex.getMessage(), ex);
     return ApiResponse.error("Duplicate: " + ex.getMessage(), HttpStatus.CONFLICT);
   }
 }
