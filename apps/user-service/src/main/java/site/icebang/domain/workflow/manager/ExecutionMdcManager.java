749a6c1c1117801944dfdb5ff83fb0289a5b3762
JiHoon <fair0838@gmail.com>
2025-09-24 17:52:14 +0900

    fix : trace_id MDC에 추가

--- apps/user-service/src/main/java/site/icebang/domain/workflow/manager/ExecutionMdcManager.java
+++ apps/user-service/src/main/java/site/icebang/domain/workflow/manager/ExecutionMdcManager.java
@@ -7,6 +7,13 @@ import org.springframework.stereotype.Component;
 public class ExecutionMdcManager {
   private static final String SOURCE_ID = "sourceId";
   private static final String EXECUTION_TYPE = "executionType";
+  private static final String TRACE_ID = "traceID";
+
+  public void setWorkflowContext(Long workflowId, String traceId) {
+    MDC.put(SOURCE_ID, workflowId.toString());
+    MDC.put(EXECUTION_TYPE, "WORKFLOW");
+    MDC.put(TRACE_ID, traceId);
+  }
 
   public void setWorkflowContext(Long workflowId) {
     MDC.put(SOURCE_ID, workflowId.toString());
