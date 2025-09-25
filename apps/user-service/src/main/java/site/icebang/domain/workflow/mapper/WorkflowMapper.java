98c92eeeada19f6438e891910ca93f26177d6d58
bwnfo3 <bwnfo0702@gmail.com>
2025-09-25 15:45:07 +0900

    feat: Workflow생성 관련 메서드, sql 추가

--- apps/user-service/src/main/java/site/icebang/domain/workflow/mapper/WorkflowMapper.java
+++ apps/user-service/src/main/java/site/icebang/domain/workflow/mapper/WorkflowMapper.java
@@ -6,12 +6,22 @@ import java.util.*;
 import site.icebang.common.dto.PageParams;
 import site.icebang.domain.workflow.dto.ScheduleDto;
 import site.icebang.domain.workflow.dto.WorkflowCardDto;
+import site.icebang.domain.workflow.dto.WorkflowCreateDto;
 import site.icebang.domain.workflow.dto.WorkflowDetailCardDto;
 
 public interface WorkflowMapper {
   List<WorkflowCardDto> selectWorkflowList(PageParams pageParams);
 
   int selectWorkflowCount(PageParams pageParams);
+
+  int insertWorkflow(Map<String, Object> params); // insert workflow
+
+  // Job 생성 관련 메서드
+  void insertDefaultJobs(Map<String, Object> params);
+  void insertJobTask(Map<String, Object> params);
+  void insertWorkflowJob(Map<String, Object> params);
+
+  boolean existsByName(String name);
 
   WorkflowCardDto selectWorkflowById(BigInteger id);
 
