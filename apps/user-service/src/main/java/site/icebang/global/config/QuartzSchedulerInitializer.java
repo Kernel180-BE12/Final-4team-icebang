7e172e19f785e46e53a391184f97b3197b6fdc01
jihukimme <jihu0210@naver.com>
2025-09-25 18:28:35 +0900

    refactor: ApplicationListener로 변경

--- apps/user-service/src/main/java/site/icebang/global/config/QuartzSchedulerInitializer.java
+++ apps/user-service/src/main/java/site/icebang/global/config/QuartzSchedulerInitializer.java
@@ -3,6 +3,8 @@ package site.icebang.global.config;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 import site.icebang.domain.schedule.model.Schedule;
 import site.icebang.domain.schedule.mapper.ScheduleMapper;
@@ -12,13 +14,13 @@ import java.util.List;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class QuartzSchedulerInitializer implements CommandLineRunner {
+public class QuartzSchedulerInitializer implements ApplicationListener<ContextRefreshedEvent> {
 
     private final ScheduleMapper scheduleMapper;
     private final QuartzScheduleService quartzScheduleService;
 
     @Override
-    public void run(String... args) {
+    public void onApplicationEvent(ContextRefreshedEvent event) {
         log.info("Quartz 스케줄러 초기화 시작: DB 스케줄을 등록합니다.");
         try {
             List<Schedule> activeSchedules = scheduleMapper.findAllActive();
