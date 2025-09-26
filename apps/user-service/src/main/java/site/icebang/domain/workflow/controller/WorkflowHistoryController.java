c6d4cd0d661b4b4aafe78ae9db530b76fde6c98e
can019 <jys01012@gmail.com>
2025-09-26 21:34:34 +0900

    feat: Execution log api

--- apps/user-service/src/main/java/site/icebang/domain/workflow/controller/WorkflowHistoryController.java
+++ apps/user-service/src/main/java/site/icebang/domain/workflow/controller/WorkflowHistoryController.java
@@ -1,14 +1,20 @@
 package site.icebang.domain.workflow.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.*;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 import site.icebang.common.dto.ApiResponse;
 import site.icebang.common.dto.PageParams;
 import site.icebang.common.dto.PageResult;
+import site.icebang.domain.log.service.ExecutionLogService;
 import site.icebang.domain.workflow.dto.WorkflowHistoryDTO;
 import site.icebang.domain.workflow.dto.WorkflowRunDetailResponse;
+import site.icebang.domain.workflow.dto.log.ExecutionLogSimpleDto;
+import site.icebang.domain.workflow.dto.log.WorkflowLogQueryCriteria;
 import site.icebang.domain.workflow.service.WorkflowHistoryService;
 
 @RestController
@@ -16,6 +22,7 @@ import site.icebang.domain.workflow.service.WorkflowHistoryService;
 @RequiredArgsConstructor
 public class WorkflowHistoryController {
   private final WorkflowHistoryService workflowHistoryService;
+  private final ExecutionLogService executionLogService;
 
   @GetMapping("")
   public ApiResponse<PageResult<WorkflowHistoryDTO>> getWorkflowHistoryList(
@@ -34,5 +41,12 @@ public class WorkflowHistoryController {
   public ApiResponse<WorkflowRunDetailResponse> getWorkflowRunDetail(@PathVariable Long runId) {
     WorkflowRunDetailResponse response = workflowHistoryService.getWorkflowRunDetail(runId);
     return ApiResponse.success(response);
+  }
+
+  @GetMapping("/logs")
+  public ApiResponse<List<ExecutionLogSimpleDto>> getTaskExecutionLog(
+      @Valid @ModelAttribute WorkflowLogQueryCriteria requestDto) {
+    return ApiResponse.success(
+        ExecutionLogSimpleDto.from(executionLogService.getRawLogs(requestDto)));
   }
 }
